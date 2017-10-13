de1cacd4bc1956dc6c644c01bacd62b2e7a657cc
stacietaylorcima <stacietaylorcima@gmail.com>
2017-10-12 18:59:15 -0700

    add .ready() method to all js files and map artist and release info to index.html with jQuery

---
+++ scripts/album-info.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('h2.artist').text(album.artist);
+  $('div#release-info').text(album.releaseInfo);
+});
