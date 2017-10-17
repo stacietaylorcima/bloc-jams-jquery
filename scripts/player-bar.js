3d8136430d99b7f14eb03935353985e075dfd628
stacietaylorcima <stacietaylorcima@gmail.com>
2017-10-17 11:28:38 -0700

    implement Previous Button functionality

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -12,6 +12,15 @@ $(document).ready(function() {
       if (nextSongIndex > album.songs.length) {return ;}
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
-
   });
+
+  $('button#previous').click(function(){
+    if (player.playState !== 'playing') {return ;}
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex < 0) {return ;}
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
+
 });
